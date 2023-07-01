c997bb1710939856a43983bf069d67545026c990
Gabituts <gabrielbastias25@gmail.com>
2023-06-30 23:30:59 -0300

    creacion de component item y carpeta models, dentro de models creado item.ts

---
+++ src/app/components/items/items.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Item } from "../../models/item"
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-items',
+  templateUrl: './items.component.html',
+  styleUrls: ['./items.component.css']
+})
+
+export class ItemsComponent implements OnInit{
+  
+  items: Item[] = [];
+
+  constructor() {}
+  ngOnInit(): void{
+    this.items = [
+      {
+        id: 0,
+        title: "pera",
+        price: 10.5,
+        quantity: 5,
+        completed: false,
+      }
+    ];
+  }
+
+}
